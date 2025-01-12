ce5d135ffa7d14fd44ae1e9f4936d824560c710b
Anusha Sahu <anushasahu0507@gmail.com>
2025-01-12 14:35:55 +0530

    Added more images and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
+import image1 from "./images/new-york-busy-street.jpg"
+import image2 from "./images/nature.jpg"
+import image3 from "./images/night-sky.jpg"
+import image4 from "./images/honeycomb.png"
+import image5 from './images/avengers.jpg'
+import image6 from './images/picasso.jpg'
 
 class App extends React.Component{
-    state = { advice: ''};
+
+    
+    state = { advice: '',
+        bgImage: image1,
+    };
 
     componentDidMount(){
         console.log(this.fetchAdvice());  
@@ -19,14 +29,24 @@ class App extends React.Component{
         .catch((error) =>{
             console.log(error);
         });
+
+    }
+
+    changeImage = () =>{
+        const images = [image1, image2, image3, image4, image5, image6];
+
+        const randomImage = images[Math.floor(Math.random() * images.length)];
+
+        this.setState({bgImage: randomImage});
     }
     render(){
-        const {advice} = this.state;
+        const {advice, bgImage} = this.state;
         return(
-            <div className="app">
+            <div className="app"
+            style={{backgroundImage: `url(${bgImage})`}}>
                 <div className="card">
                     <div className="heading">{ advice }</div>
-                    <button className="button" onClick={this.fetchAdvice}>
+                    <button className="button" onClick={ () =>{this.fetchAdvice(); this.changeImage();}}>
                         <span>GIVE ME ADVICE !</span>
                     </button>
                 </div>
